5050ac3556d5653201811bfbc1b1d62e0de56c76
Meirino Harianto <meirinoharianto@Meirinos-MacBook-Air.local>
2025-05-04 06:51:43 +0700

    login update

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -13,49 +13,50 @@
 
 <body style="background:#0c4e68;">
     <!-- <button id="full-screen">FULL SCREEN</button> -->
-
-    <div class="container">
-        <!-- grid -->
-        <div class="row">
-            <div class="col-sm-5 mx-auto mt-5 pt-5">
-                <?php if (!empty($this->session->flashdata('failed'))) { ?>
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            <span class="sr-only">Close</span>
-                        </button>
-                        <?= $this->session->flashdata('failed'); ?>
-                    </div>
-                <?php } ?>
-                <div class="card">
-                    <div class="card-header text-center">
-                        <h3><b>SARESTO POS SAM NDUT</b></h3>
-                    </div>
-                    <div class="card-body">
-                        <form method="POST" action="<?= base_url('login/proses'); ?>">
-                            <div class="form-group">
-                                <label for="">Username</label>
-                                <input type="text" required class="form-control" autocomplete="off" name="user" id="user" placeholder="Masukan Username">
-                            </div>
-                            <div class="form-group">
-                                <label for="">Password</label>
-                                <input type="password" required class="form-control" autocomplete="off" name="pass" id="pass" placeholder="Masukan Password">
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-md float-right">
-                                Masuk
+    <section class="vh-100 gradient-custom">
+        <div class="container py-5 h-100">
+            <!-- grid -->
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <?php if (!empty($this->session->flashdata('failed'))) { ?>
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                <span class="sr-only">Close</span>
                             </button>
-                        </form>
-                    </div>
-                    <div class="card-footer text-center">
-                        Copyright &copy; <?= date('Y'); ?> SARESTO
-                        </br><a href="https://www.salasatekno.com" target="_blank"><b>Salasa Teknologi Solusindo</b></a>
-                        </br>1.11.01
+                            <?= $this->session->flashdata('failed'); ?>
+                        </div>
+                    <?php } ?>
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-header text-center">
+                            <h3><b>WARMINDO SAM NDUT</b></h3>
+                        </div>
+                        <div class="card-body p-5 text-center">
+                            <form method="POST" action="<?= base_url('login/proses'); ?>">
+                                <div class="form-group">
+                                    <label for="">Username</label>
+                                    <input type="text" required class="form-control" autocomplete="off" name="user" id="user" placeholder="Masukan Username">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Password</label>
+                                    <input type="password" required class="form-control" autocomplete="off" name="pass" id="pass" placeholder="Masukan Password">
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-md float-right">
+                                    Masuk
+                                </button>
+                            </form>
+                        </div>
+                        <div class="card-footer text-center">
+                            Copyright &copy; <?= date('Y'); ?> SARESTO
+                            </br><a href="https://www.salasatekno.com" target="_blank"><b>Salasa Teknologi Solusindo</b></a>
+                            </br>1.11.01
+                        </div>
                     </div>
                 </div>
             </div>
+            <!-- grid -->
         </div>
-        <!-- grid -->
-    </div>
+    </section>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="<?= base_url('assets/js/jquery-3.3.1.min.js'); ?>"></script>
