42665e3f386a1bb21bbbe28215dd93aec2d719bb
Meirino Harianto <meirinoharianto@Meirinos-MacBook-Air.local>
2025-05-04 16:30:43 +0700

    tes

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -6,6 +6,6 @@ class Test extends CI_Controller
     public function index()
     {
         // echo "";
-        $this->load->view('login/test', $this->data);
+        $this->load->view('login/test');
     }
 }
