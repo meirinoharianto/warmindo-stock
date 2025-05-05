effbe81356a8d0d782fc23353281a5072750db34
Meirino Harianto <meirinoharianto@Meirinos-MacBook-Air.local>
2025-05-05 14:07:47 +0700

    kasir update

--- application/views/admin/kasirstok/menu.php
+++ application/views/admin/kasirstok/menu.php
@@ -1,8 +1,8 @@
 <?php foreach ($hasil as $r) { ?>
 
-    <div class="<?= count($hasil) === 1 ? 'col-lg-12' : 'col-lg-4'; ?> col-md-6 col-sm-12 mb-3">
+    <div class="<?= count($hasil) === 1 ? 'col-lg-8' : 'col-lg-4'; ?> col-md-6 col-sm-12 mb-3">
         <div class="card w-100">
-            <?= count($hasil) === 1 ? 'col-lg-12' : 'col-lg-4'; ?>
+            <?= count($hasil) === 1 ? 'col-lg-8' : 'col-lg-4'; ?>
             <!-- <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="Gambar Kolom 1"> -->
             <?php
             if ($r->gambar !== '-') {
