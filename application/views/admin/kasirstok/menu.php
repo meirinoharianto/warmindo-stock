ad0bf88eab8f4f420418898115f458b66a0128e8
Meirino Harianto <meirinoharianto@Meirinos-MacBook-Air.local>
2025-05-05 15:59:38 +0700

    kasir update

--- application/views/admin/kasirstok/menu.php
+++ application/views/admin/kasirstok/menu.php
@@ -1,7 +1,8 @@
-<?php foreach ($hasil as $r) { ?>
+<div class="col-md-6 col-sm-12 mb-3">
 
-    <!-- <div class="<?= count($hasil) === 1 ? 'col-lg-8' : 'col-lg-4'; ?> col-md-6 col-sm-12 mb-3"> -->
-    <div class="col-md-6 col-sm-12 mb-3">
+    <?php foreach ($hasil as $r) { ?>
+
+        <!-- <div class="<?= count($hasil) === 1 ? 'col-lg-8' : 'col-lg-4'; ?> col-md-6 col-sm-12 mb-3"> -->
         <div class="card w-100">
 
             <!-- <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="Gambar Kolom 1"> -->
@@ -56,9 +57,9 @@
                 <?php } ?>
             </div>
         </div>
-    </div>
 
-<?php } ?>
+    <?php } ?>
+</div>
 
 <script>
     $('.pilih').on('click', function(e) {
