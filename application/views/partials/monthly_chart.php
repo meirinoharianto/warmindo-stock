bbe3d6b6a3835589d39239e4bbbc91eb509cc7a5
meirinoharianto <meirinoharianto@gmail.com>
2025-08-26 09:21:50 +0700

    update grafk

--- application/views/partials/monthly_chart.php
+++ application/views/partials/monthly_chart.php
@@ -78,7 +78,7 @@ if ($has_monthly_data): ?>
 <?php else: ?>
     <div class="alert alert-warning text-center py-4">
         <i class="fa fa-exclamation-triangle fa-2x mb-2"></i>
-        <h5>Data tidak ditemukan <?= $thn_monthly ?> atau <?= $tes ?> atau <?= $tes2 ?></h5>
+        <h5>Data tidak ditemukan </h5>
         <p>Tidak ada data penjualan untuk filter yang dipilih</p>
     </div>
 <?php endif; ?>
