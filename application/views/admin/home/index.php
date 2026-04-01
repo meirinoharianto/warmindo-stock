2528ca57e2493e70595a5d13fffb259b6eaa2f6c
meirinoharianto <meirinoharianto@gmail.com>
2026-04-01 14:22:01 +0700

    update home admin

--- application/views/admin/home/index.php
+++ application/views/admin/home/index.php
@@ -16,7 +16,7 @@ $filter_branch_submitted = !empty($this->input->post('filter_branch'));
 $filter_stock_submitted = !empty($this->input->post('filter_stock'));
 ?>
 
-<div id="adminkasir" class="d-flex flex-column h-100">
+<div id="home" class="d-flex flex-column h-100">
     <div class="wrapper d-flex flex-grow-1">
         <div id="content" class="p-0 flex-grow-1">
             <div class="container-fluid h-100">
