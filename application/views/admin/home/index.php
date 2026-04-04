4e8d91cbb65a11bb605e267a4eb0cbe0ebcffbce
meirinoharianto <meirinoharianto@gmail.com>
2026-04-04 12:44:52 +0700

    update semua cabang

--- application/views/admin/home/index.php
+++ application/views/admin/home/index.php
@@ -8,7 +8,7 @@ $idcabang_monthly = !empty($this->input->post('idcabang_monthly')) ? $this->inpu
 
 $thn_stock = !empty($this->input->post('thn_stock')) ? $this->input->post('thn_stock') : date('Y');
 $bln_stock = !empty($this->input->post('bln_stock')) ? $this->input->post('bln_stock') : date('m');
-$idcabang_stock = !empty($this->input->post('idcabang_stock')) ? $this->input->post('idcabang_stock') : 4;
+$idcabang_stock = !empty($this->input->post('idcabang_stock')) ? $this->input->post('idcabang_stock') : 0;
 $idbahan_stock = !empty($this->input->post('idbahan_stock')) ? $this->input->post('idbahan_stock') : 0;
 
 // Check which form was submitted
