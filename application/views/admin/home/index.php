df22091ec15bf3d4775819c52c50e4791ac4f432
meirinoharianto <meirinoharianto@gmail.com>
2026-04-05 17:04:14 +0700

    reset menu sales chart

--- application/views/admin/home/index.php
+++ application/views/admin/home/index.php
@@ -240,12 +240,12 @@ $filter_stock_submitted = !empty($this->input->post('filter_stock'));
                                         <div id="stock-chart-container">
                                             <?php
                                             // Load branch chart partial view
-                                            // $this->load->view('partials/stock_chart', [
-                                            //     'thn_stock' => $thn_stock,
-                                            //     'bln_stock' => $bln_stock,
-                                            //     'idcabang_stock' => $idcabang_stock,
-                                            //     'filter_stock_submitted' => $filter_stock_submitted
-                                            // ]);
+                                            $this->load->view('partials/stock_chart', [
+                                                'thn_stock' => $thn_stock,
+                                                'bln_stock' => $bln_stock,
+                                                'idcabang_stock' => $idcabang_stock,
+                                                'filter_stock_submitted' => $filter_stock_submitted
+                                            ]);
                                             ?>
                                         </div>
                                     </div>
@@ -339,7 +339,7 @@ $filter_stock_submitted = !empty($this->input->post('filter_stock'));
                                         <div id="stock2-chart-container">
                                             <?php
                                             // Load branch chart partial view
-                                            $this->load->view('partials/stock_chart', [
+                                            $this->load->view('partials/stock_chart2', [
                                                 'thn_stock' => $thn_stock,
                                                 'bln_stock' => $bln_stock,
                                                 'idcabang_stock' => $idcabang_stock,
