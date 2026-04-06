3c93defdc7aae8c0f2d1f950b9dfdcd46a2d6cbf
meirinoharianto <meirinoharianto@gmail.com>
2026-04-06 16:42:21 +0700

    add menu sales chart

--- application/views/partials/menu_sales_chart.php
+++ application/views/partials/menu_sales_chart.php
@@ -110,7 +110,7 @@ if ($cabang != -1) {
         $menu_sales_labels[] = $label_with_qty;
 
         ?>
-        <!-- <p><?= $this->db->last_query(); ?> </br> <?= $menu->nama . ' = ' . $total_qty ?></br> data :<?= count($menu_sales_data) ?></p> -->
+        <p><?= $this->db->last_query(); ?> </br> <?= $menu->nama . ' = ' . $total_qty ?></br> data :<?= count($menu_sales_data) ?></p>
 
     <?php
 
