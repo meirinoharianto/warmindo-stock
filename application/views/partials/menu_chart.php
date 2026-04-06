d62194b12e0caead6a4a84d0104044f777519062
meirinoharianto <meirinoharianto@gmail.com>
2026-04-06 17:53:26 +0700

    add menu sales chart

--- application/views/partials/menu_chart.php
+++ application/views/partials/menu_chart.php
@@ -217,6 +217,6 @@ if ($has_menu_data): ?>
         <i class="fa fa-exclamation-triangle fa-2x mb-2"></i>
         <h5>Data tidak ditemukan</h5>
         <!-- <p><?= $sql . $period_stock; ?></p> -->
-        <p>Tidak ada data penjualan untuk bulan <?= $bulan[$bln_menu] ?? '' ?> <?= $thn_menu ?></p>
+        <p>Tidak ada data penjualan untuk cabang <?= $cabang ?> bulan <?= $bulan[$bln_menu] ?? '' ?> <?= $thn_menu ?></p>
     </div>
 <?php endif; ?>
