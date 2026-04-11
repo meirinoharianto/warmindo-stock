ae9fd2869d41e3a27055660479e85e2ab1b73702
meirinoharianto <meirinoharianto@gmail.com>
2026-04-11 19:33:42 +0700

    add Koordinator

--- application/views/admin/coordinators/edit.php
+++ application/views/admin/coordinators/edit.php
@@ -46,7 +46,7 @@
                             <div class="pull-right">
                                 <input type="hidden" class="form-control" value="<?= $user->id; ?>" name="id">
                                 <button type="submit" class="btn btn-primary btn-md">
-                                    <b><i class="fa fa-edit"></i> Simpan</b></button>
+                                    <b><i class="fa fa-edit"></i> Simpan </b></button>
                                 <a href="<?= base_url('coordinators'); ?>" class="btn btn-danger btn-md">
                                     <b><i class="fa fa-angle-double-left"></i> Kembali</b></a>
                             </div>
