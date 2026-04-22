e60bedb2c47708b238e8d691ea36a29f981071c8
meirinoharianto <meirinoharianto@gmail.com>
2026-04-22 15:51:16 +0700

    fix koordinator

--- application/controllers/Coordinators.php
+++ application/controllers/Coordinators.php
@@ -214,7 +214,7 @@ class Coordinators extends CI_Controller
             'telepon' => $telepon,
             'alamat' => $alamat
         );
-        $this->db->update_table('login', 'id', $id, $data);
+        $this->M_Admin->update_table('login', 'id', $id, $data);
         $this->session->set_flashdata('success', 'Berhasil Update Koordinator : ' . $nama . ' !');
         redirect(base_url('coordinators/edit/' . $id));
         // } else {
