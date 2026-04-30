edf6ae05a5adeab8b8e37e30ee1cfddd65c7410c
meirinoharianto <meirinoharianto@gmail.com>
2026-04-30 18:26:43 +0700

    update menu grafik stok keluar bahan

--- application/views/admin/adminkasir/index.php
+++ application/views/admin/adminkasir/index.php
@@ -6,20 +6,13 @@ $thn_branch = !empty($this->input->post('thn_branch')) ? $this->input->post('thn
 $bln_branch = !empty($this->input->post('bln_branch')) ? $this->input->post('bln_branch') : date('m');
 $idcabang_monthly = !empty($this->input->post('idcabang_monthly')) ? $this->input->post('idcabang_monthly') : 4;
 
-$thn_stock = !empty($this->input->post('thn_stock')) ? $this->input->post('thn_stock') : date('Y');
-$bln_stock = !empty($this->input->post('bln_stock')) ? $this->input->post('bln_stock') : date('m');
-$idcabang_stock = !empty($this->input->post('idcabang_stock')) ? $this->input->post('idcabang_stock') : -1;
-$idbahan_stock = !empty($this->input->post('idbahan_stock')) ? $this->input->post('idbahan_stock') : 0;
-
 $thn_stock2 = !empty($this->input->post('thn_stock2')) ? $this->input->post('thn_stock2') : date('Y');
 $bln_stock2 = !empty($this->input->post('bln_stock2')) ? $this->input->post('bln_stock2') : date('m');
 $idcabang_stock2 = !empty($this->input->post('idcabang_stock2')) ? $this->input->post('idcabang_stock2') : -1;
 $idbahan_stock2 = !empty($this->input->post('idbahan_stock2')) ? $this->input->post('idbahan_stock2') : 0;
 
 // Check which form was submitted
-$filter_monthly_submitted = !empty($this->input->post('filter_monthly'));
 $filter_branch_submitted = !empty($this->input->post('filter_branch'));
-$filter_stock_submitted = !empty($this->input->post('filter_stock'));
 $filter_stock2_submitted = !empty($this->input->post('filter_stock2'));
 
 
@@ -112,94 +105,7 @@ $bulan = array(
 
 
                                 <!-- Chart 3: Stock Chart by Branch by Month-->
-                                <div class="row mb-4">
-                                    <div class="col-12 border rounded-lg p-3">
-                                        <div class="d-flex justify-content-between align-items-center mb-3">
-                                            <h5>Stok Keluar Bahan per Cabang per Bulan</h5>
-                                            <form method="post" action="<?= base_url('adminkasir') ?>" class="form-inline" id="stock-filter-form">
-                                                <div class="d-flex align-items-center">
-                                                    <input type="hidden" name="thn_stock" value="<?= $thn_stock ?>">
-                                                    <div class="mr-2">
-                                                        <select name="idcabang_stock" class="form-control form-control-sm">
-                                                            <option value="all">- Semua Cabang -</option>
-                                                            <?php
 
-                                                            foreach ($namacabang as $r) {
-                                                            ?>
-                                                                <option value="<?= $r->cabang_id; ?>" <?= ($idcabang_stock == $r->cabang_id) ? 'selected' : '' ?>>
-                                                                    <?= $r->nama_toko; ?>
-                                                                </option>
-                                                            <?php } ?>
-                                                        </select>
-                                                    </div>
-                                                    <div class="mr-2">
-                                                        <select name="bln_stock" class="form-control form-control-sm">
-                                                            <?php
-
-                                                            foreach ($bulan as $key => $value) {
-                                                            ?>
-                                                                <option value="<?= $key ?>" <?= ($bln_stock == $key) ? 'selected' : '' ?>>
-                                                                    <?= $value ?>
-                                                                </option>
-                                                            <?php } ?>
-                                                        </select>
-                                                    </div>
-                                                    <div class="mr-2">
-                                                        <select name="thn_stock" class="form-control form-control-sm">
-                                                            <?php
-                                                            $thn_skr = date('Y');
-                                                            for ($x = $thn_skr; $x >= 2021; $x--) {
-                                                            ?>
-                                                                <option value="<?= $x; ?>" <?= ($thn_stock == $x) ? 'selected' : '' ?>>
-                                                                    <?= $x; ?>
-                                                                </option>
-                                                            <?php } ?>
-                                                        </select>
-                                                    </div>
-                                                    <div class="mr-2">
-                                                        <select name="idbahan_stock[]" class="form-control form-control-sm" multiple size="6">
-                                                            <option value="0">- Semua Bahan -</option>
-                                                            <?php
-                                                            $this->db->order_by('nama_bahan', 'asc');
-                                                            $namabahan = $this->db->get('bahan')->result();
-                                                            foreach ($namabahan as $n) {
-                                                            ?>
-                                                                <option value="<?= $n->id; ?>"
-                                                                    <?= (isset($idbahan_stock) && in_array($n->id, (array)$idbahan_stock)) ? 'selected' : '' ?>>
-                                                                    <?= $n->nama_bahan; ?>
-                                                                </option>
-                                                            <?php } ?>
-                                                        </select>
-                                                    </div>
-                                                    <button type="submit" name="filter_stock" class="btn btn-primary btn-sm">
-                                                        <i class="fa fa-filter"></i> Filter
-                                                    </button>
-                                                </div>
-                                            </form>
-                                        </div>
-
-                                        <div id="chart-wrapper" class="chart-loading-box">
-                                            <div class="chart-loading-overlay">
-                                                <div class="chart-loading-spinner"></div>
-                                                <div class="chart-loading-text">Memuat grafik...</div>
-                                            </div>
-
-                                            <div id="stock-chart-container">
-                                                <?php
-                                                // Load branch chart partial view
-                                                $this->load->view('partials/stock_chart', [
-                                                    'thn_stock' => $thn_stock,
-                                                    'bln_stock' => $bln_stock,
-                                                    'idcabang_stock' => $idcabang_stock,
-                                                    'filter_stock_submitted' => $filter_stock_submitted
-                                                ]);
-                                                ?>
-                                            </div>
-                                        </div>
-
-
-                                    </div>
-                                </div>
 
                                 <!-- Chart 4: Menu Sales Chart by Branch by Month-->
                                 <div class="row mb-4">
@@ -300,20 +206,7 @@ $bulan = array(
 
 <script>
     $(document).ready(function() {
-        // AJAX form submission for monthly filter
-        $('#monthly-filter-form').submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-                url: $(this).attr('action'),
-                type: 'POST',
-                data: $(this).serialize(),
-                success: function(response) {
-                    // Extract just the monthly chart container content from the response
-                    var newContent = $(response).find('#monthly-chart-container').html();
-                    $('#monthly-chart-container').html(newContent);
-                }
-            });
-        });
+
 
         // AJAX form submission for branch filter
         $('#branch-filter-form').submit(function(e) {
@@ -331,20 +224,7 @@ $bulan = array(
         });
 
 
-        // AJAX form submission for stock filter
-        // $('#stock-filter-form').submit(function(e) {
-        //     e.preventDefault();
-        //     $.ajax({
-        //         url: $(this).attr('action'),
-        //         type: 'POST',
-        //         data: $(this).serialize(),
-        //         success: function(response) {
-        //             // Extract just the branch chart container content from the response
-        //             var newContent = $(response).find('#stock-chart-container').html();
-        //             $('#stock-chart-container').html(newContent);
-        //         }
-        //     });
-        // });
+
 
         // AJAX form submission for stock filter
         $('#stock2-filter-form').submit(function(e) {
@@ -375,48 +255,9 @@ $bulan = array(
                 }
             });
         });
-        // $('#stock2-filter-form').submit(function(e) {
-        //     e.preventDefault();
-        //     $.ajax({
-        //         url: $(this).attr('action'),
-        //         type: 'POST',
-        //         data: $(this).serialize(),
-        //         success: function(response) {
-        //             // Extract just the branch chart container content from the response
-        //             var newContent = $(response).find('#stock2-chart-container').html();
-        //             $('#stock2-chart-container').html(newContent);
-        //         }
-        //     });
-        // });
 
-        $('#stock-filter-form').submit(function(e) {
-            var wrapper = $('#chart-wrapper');
-            var result = $('#stock-chart-container');
 
-            wrapper.addClass('loading');
 
-            $.ajax({
-                url: $(this).attr('action'),
-                type: 'POST',
-                data: $(this).serialize(),
-                success: function(res) {
-                    // result.html(res);
-                    // Extract just the branch chart container content from the response
-                    var newContent = $(response).find('#stock-chart-container').html();
-                    $('#stock-chart-container').html(newContent);
-                },
-                error: function() {
-                    result.html(`
-                <div class="alert alert-danger text-center py-3">
-                    Gagal memuat grafik.
-                </div>
-            `);
-                },
-                complete: function() {
-                    wrapper.removeClass('loading');
-                }
-            });
-        });
 
     });
 </script>
