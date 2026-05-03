68b757436a42f0bf5d9a7d978190416922f6befa
meirinoharianto <meirinoharianto@gmail.com>
2026-05-03 18:17:23 +0700

    move grafik menu terjual

--- application/views/admin/adminkasir/grafik/menu-terjual.php
+++ application/views/admin/adminkasir/grafik/menu-terjual.php
@@ -117,7 +117,6 @@ $bulan = array(
                                             </div>
                                             <div id="stock2-chart-container">
                                                 <?php
-                                                // if ($idcabang_stock2 >= 0) {
                                                 // Load branch chart partial view
                                                 $this->load->view('partials/stock2_chart', [
                                                     'thn_stock2' => $thn_stock2,
@@ -125,13 +124,6 @@ $bulan = array(
                                                     'idcabang_stock2' => $idcabang_stock2,
                                                     'filter_stock2_submitted' => $filter_stock2_submitted
                                                 ]);
-                                                // } else {
-                                                ?>
-                                                <!-- <div class="text-center text-muted py-4">
-                                                        Silakan pilih filter lalu klik "Filter" untuk menampilkan grafik.
-                                                    </div> -->
-                                                <?php
-                                                //  } 
                                                 ?>
                                             </div>
                                         </div>
