e7eb46312c1d962f9cb67e7f98d95ebe26c93efb
meirinoharianto <meirinoharianto@gmail.com>
2026-05-07 07:56:09 +0700

    move grafik

--- application/views/partials/branch_chart.php
+++ application/views/partials/branch_chart.php
@@ -1,4 +1,18 @@
 <?php
+$bulan = array(
+    '01' => 'Januari',
+    '02' => 'Februari',
+    '03' => 'Maret',
+    '04' => 'April',
+    '05' => 'Mei',
+    '06' => 'Juni',
+    '07' => 'Juli',
+    '08' => 'Agustus',
+    '09' => 'September',
+    '10' => 'Oktober',
+    '11' => 'November',
+    '12' => 'Desember'
+);
 // Prepare branch data
 
 $branch_data = [];
