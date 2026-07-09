d5106b037da76a7256c8e5ed95ac53434c14f469
meirinoharianto <meirinoharianto@gmail.com>
2026-07-09 12:20:38 +0700

    tambah header transferstok multi

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -201,6 +201,8 @@
                                             <i class="fa fa-cubes pr-1"></i> Menu Utama</a>
                                         <a class="dropdown-item" href="<?= base_url('bahan'); ?>">
                                             <i class="fa fa-cubes pr-1"></i> Bahan</a>
+                                        <a class="dropdown-item" href="<?= base_url('bahan/transferstok_multi'); ?>"><i class="fa fa-truck pr-1"></i> Transfer Stok Multi Cabang</a>
+
                                         <a class="dropdown-item" href="<?= base_url('coordinators'); ?>">
                                             <i class="fa fa-cubes pr-1"></i> Koordinator</a>
                                         <!-- <a class="dropdown-item" href="<?= base_url('bahanbaku'); ?>">
